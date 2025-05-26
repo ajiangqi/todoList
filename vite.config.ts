c02f7f8dc527c38b2483b27ea7b6350198015075
huzixiang <1820341513@qq.com>
2025-05-26 09:07:49 +0800

    todolist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /** WARNING: DON'T EDIT THIS FILE */
 /** WARNING: DON'T EDIT THIS FILE */
 /** WARNING: DON'T EDIT THIS FILE */
-
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -13,4 +13,9 @@ function getPlugins() {
 
 export default defineConfig({
   plugins: getPlugins(),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),  // 使用绝对路径
+    },
+  },
 });
